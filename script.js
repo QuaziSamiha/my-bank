0aa2efafb69fc5f658d0dc3f1ee86e5271832182
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-01-29 12:48:08 +0600

    deposit section 90% done

--- script.js
+++ script.js
@@ -15,8 +15,16 @@ depositBtn.addEventListener('click', function(){
     const currentDeposit = document.getElementById('current-deposit').innerText;
     const currentDepositNumber = parseFloat(currentDeposit);
     const totalDeposit = depositAmountNumber + currentDepositNumber;
+    document.getElementById('current-deposit').innerText = totalDeposit;
     
-    document.getElementById('current-deposit').innerText = totalDeposit; 
+    const currentBalance = document.getElementById('current-balance').innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = depositAmountNumber + currentBalanceNumber;
+    document.getElementById('current-balance').innerText = totalBalance;
     
     document.getElementById('deposit-amount').value = "";
 })
+
+// function depositSection(){
+
+// }
